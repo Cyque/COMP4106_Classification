7e7e99de66b16d03bbeb16a2914ed4c22959f534
cyque <damian.polan@gmail.com>
2015-03-25 22:17:51 -0400

    Added class generation

---
+++ COMP4106_Assignment3/Classification/FeatureClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace COMP4106_Assignment3.Classification
+{
+    public class FeatureClass
+    {
+        public Dictionary<String, int> features;
+
+        public FeatureClass()
+        {
+            features = new Dictionary<string, int>();
+        }
+
+        public void setFeature(String featureName, int value)
+        {
+            if (features.ContainsKey(featureName))
+                features[featureName] = value;
+            else
+                features.Add(featureName, value);
+        }
+
+
+        public override string ToString()
+        {
+            String s = "";
+
+            foreach(KeyValuePair<string, int> entry in features){
+                s += entry.Key + ": " + entry.Value + "\n";
+            }
+
+            return s;
+        }
+
+
+
+    }
+}
---
+++ COMP4106_Assignment3/MainForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using COMP4106_Assignment3.Classification;
+
+namespace COMP4106_Assignment3
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerateData_Click(object sender, EventArgs e)
+        {
+            /*                  0    1
+             *             n1 (0.9, 0.9)
+             *     n2 (0.9, 0.1)    n3 (0.1, 0.9)
+             * 
+             *  n1=1 -> n2 will prob be 1, n2 -> 0, n2 will prob be 1
+             * 
+             */
+
+            DecisionNode n1 = new DecisionNode(null, 0d, 0.9d, "n1");
+            DecisionNode n2 = new DecisionNode(n1, 1d, 0.0d, "n2");
+            DecisionNode n3 = new DecisionNode(n1, 1d, 0d, "n3");
+
+            FeatureClass fClass = n1.generateClass();
+
+            Console.WriteLine(fClass.ToString());
+        }
+    }
+}
